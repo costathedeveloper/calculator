8c4fb902eea39b481446a0e26720536df424f5f7
costawhitehat <95866232+costawhitehat@users.noreply.github.com>
2023-03-31 10:20:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+const http = require('http');
 const readline = require('readline-sync');
 
-console.log('Welcome to the calculator app!');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('Welcome to the calculator app!\n\n');
 
-const num1 = readline.questionInt('Enter the first number: ');
-const num2 = readline.questionInt('Enter the second number: ');
+  const num1 = readline.questionInt('Enter the first number: ');
+  const num2 = readline.questionInt('Enter the second number: ');
 
-console.log(`The sum of ${num1} and ${num2} is ${num1 + num2}.`);
-console.log(`The difference between ${num1} and ${num2} is ${num1 - num2}.`);
-console.log(`The product of ${num1} and ${num2} is ${num1 * num2}.`);
-console.log(`The quotient of ${num1} and ${num2} is ${num1 / num2}.`);
+  res.write(`The sum of ${num1} and ${num2} is ${num1 + num2}.\n`);
+  res.write(`The difference between ${num1} and ${num2} is ${num1 - num2}.\n`);
+  res.write(`The product of ${num1} and ${num2} is ${num1 * num2}.\n`);
+  res.write(`The quotient of ${num1} and ${num2} is ${num1 / num2}.\n`);
+  res.end();
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
